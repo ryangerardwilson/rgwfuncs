1586d54f76aff4c4ce54985208b636b378b6d882
Ryan Gerard Wilson <ryangerardwilson@gmail.com>
2025-06-17 04:44:36 +0530

    sync

--- tests/str_lib/test_send_telegram_message.py
+++ tests/str_lib/test_send_telegram_message.py
@@ -11,8 +11,8 @@ from src.rgwfuncs.str_lib import send_telegram_message
 
 
 # Telegram credentials
-BOT_TOKEN = "7450388541:AAFvy4rpcF3e6gDb1NqDwo0WG7Q_ixdtLnQ"
-CHAT_ID = "7214830293"
+BOT_TOKEN = ""
+CHAT_ID = ""
 
 send_telegram_message("wow", bot_token=BOT_TOKEN, chat_id=CHAT_ID)
 send_telegram_message("ahem", bot_token=BOT_TOKEN, chat_id=CHAT_ID)
